ce5bc6680cbb71411f5c09203ef5ff6b017a1901
Imtiyaj Sayyad <imsayyad97@gmail.com>
2023-10-23 23:29:22 +0530

    Skills section updating styles

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -11,7 +11,7 @@ import CSS3 from "../../images/css3.png";
 import JavaScript from "../../images/javascript.png";
 import Sass from "../../images/sass.png";
 
-import "./style.css";
+import "./style.scss";
 const Skills = () => {
   return (
     <section
@@ -24,26 +24,49 @@ const Skills = () => {
                     <div className="card">
                         <div className="box">
                             <div className="text">FrameWorks</div>
-                            <img src={Next} />
-                            <img src={ReactJS} /><br/>
-                            <img src={NodeJS} />
-                            <img src={ExpressJS} />
+                            <a href="https://nextjs.org/" target='_blank'>
+                                <img src={Next} />
+                            </a>
+                            <a href="https://react.dev/" target='_blank'>
+                                <img src={ReactJS} />
+                            </a><br/>
+                            <a href="https://nodejs.org/en" target='_blank'>
+                                <img src={NodeJS} />
+                            </a>
+                            <a href="https://expressjs.com/" target='_blank'>
+                                <img src={ExpressJS} />
+                            </a>
+                            
                         </div>
                     </div>
                     <div className="card">
                         <div className="box">
                             <div className="text">Hosting Platforms</div>
-                            <img src={GitHub} /><br/>
-                            <img src={Netlify} />
+                            <a href="https://pages.github.com/" target='_blank'>
+                                <img src={GitHub} />
+                            </a><br/>
+                            <a href="https://www.netlify.com/" target='_blank'>
+                                <img src={Netlify} />
+                            </a>
+                            
                         </div>
                     </div>
                     <div className="card">
                         <div className="box">
                             <div className="text">Programming Languages</div>
-                            <img src={HTML5} alt="" />
-                            <img src={CSS3} alt="" />
-                            <img src={JavaScript} alt="" />
-                            <img src={Sass} alt="" />
+                            <a href="https://www.w3schools.com/html/" target='_blank'>
+                                <img src={HTML5} alt="" />
+                            </a>
+                            <a href="https://www.css3.com/" target='_blank'>
+                                <img src={CSS3} alt="" />
+                            </a>
+                            <a href="https://www.javascript.com/" target='_blank'>
+                                <img src={JavaScript} alt="" />
+                            </a>
+                            <a href="https://sass-lang.com/" target='_blank'>
+                                <img src={Sass} alt="" />
+                            </a>
+                            
                         </div>
                     </div>
                 </div>

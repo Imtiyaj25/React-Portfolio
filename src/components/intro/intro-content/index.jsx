d9fa6c566fed78bb7750423b289cf10e7ebf551f
Imtiyaj Sayyad <imsayyad97@gmail.com>
2023-10-24 12:46:35 +0530

    intro section

--- src/components/intro/intro-content/index.jsx
+++ src/components/intro/intro-content/index.jsx
@@ -19,7 +19,7 @@ const IntroContent = () => {
     return (
         <div className="intro-content">
             <div className="layout">
-                <div className="left-col">
+                <div className="col">
                     <h1 className="title">
                         <span className="small-text">
                             <span className="text">Hello</span>
@@ -31,41 +31,7 @@ const IntroContent = () => {
                         <span className="big-text">Imtiyaj Sayyad</span>
                         <span className="text-1"><span className="typing">{text}</span><Cursor/></span>
                     </h1>
-                    {/* <p className="title">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500
-                    </p> */}
-                    {/* <CallToAction
-                        text="Contact me"
-                        action={() => {
-                            scrollToSection("contact");
-                        }}
-                    /> */}
                 </div>
-                {/* <div className="right-col">
-                    <img
-                        src={girl}
-                        alt="Hello I am Keniya Andrew"
-                    />
-
-                    <div className="highlights horizontal">
-                        <div className="icon">
-                            <BsAwardFill />
-                        </div>
-                        <div className="text">Best Design Award</div>
-                    </div>
-
-                    <div className="highlights verticle">
-                        <div className="icon">
-                            <FaUser />
-                        </div>
-                        <div className="text">
-                            <span>4k+</span>
-                            Happy Customers
-                        </div>
-                    </div>
-                </div> */}
             </div>
         </div>
     );

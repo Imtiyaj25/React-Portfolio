c6d2e0a45afb36a44e7574d69229bd7a7cb56179
Imtiyaj Sayyad <imsayyad97@gmail.com>
2024-01-03 23:11:30 +0530

    Updating Skills section

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -23,19 +23,21 @@ const Skills = () => {
                 <div className="skills-content">
                     <div className="card">
                         <div className="box">
-                            <div className="text">FrameWorks</div>
-                            <a rel="noreferrer" href="https://nextjs.org/" target='_blank'>
-                                <img src={Next} alt='something wrong'/>
-                            </a>
-                            <a rel="noreferrer" href="https://react.dev/" target='_blank'>
-                                <img src={ReactJS} alt='something wrong'/>
-                            </a><br/>
-                            <a rel="noreferrer" href="https://nodejs.org/en" target='_blank'>
-                                <img src={NodeJS} alt='something wrong'/>
-                            </a>
-                            <a rel="noreferrer" href="https://expressjs.com/" target='_blank'>
-                                <img src={ExpressJS} alt='something wrong'/>
-                            </a>
+                            <div className="text">Front-End</div>
+                            <div className="items">
+                                <a rel="noreferrer" href="https://nextjs.org/" target='_blank'>
+                                    <img src={Next} alt='something wrong'/><p>Next JS</p>
+                                </a>
+                                <a rel="noreferrer" href="https://react.dev/" target='_blank'>
+                                    <img src={ReactJS} alt='something wrong'/><p>React JS</p>
+                                </a><br/>
+                                <a rel="noreferrer" href="https://nodejs.org/en" target='_blank'>
+                                    <img src={NodeJS} alt='something wrong'/><p>Node JS</p>
+                                </a>
+                                <a rel="noreferrer" href="https://expressjs.com/" target='_blank'>
+                                    <img src={ExpressJS} alt='something wrong'/><p>Express JS</p>
+                                </a>
+                            </div>
                             
                         </div>
                     </div>
